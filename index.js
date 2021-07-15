6cb733ef1d2e553515cad75c2410d09db5a3098a
Corey <corey.amano@gmail.com>
2021-07-14 18:03:50 -0700

    add webpack

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/* math */
-const moment = require('moment');
+/* moment */
+
+var moment = require('moment'); //require
 moment().format();
 
 console.log("Greetings from JS!");
 
-console.log(moment().startOf('day').fromNow())
+console.log(moment().startOf('day').fromNow());
+
+var math = require('mathjs');
+console.log(math.sqrt(-4).toString());
